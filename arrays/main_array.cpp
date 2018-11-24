4f1aec8a277c1f609cefd0cc2633ec9d6d52370f
linde <yuyang@udel.edu>
2018-11-24 17:12:14 -0500

    init array

---
+++ arrays/main_array.cpp
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main(){
+    
+    std::cout<<"Begin the testing for arrays!"<<std::endl;
+
+    return 0;
+}
